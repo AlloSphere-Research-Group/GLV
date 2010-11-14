fbf5eced1753c666621d0ccf09dbc75c327ccc0e
l.putnam <l.putnam@db259aed-472b-0410-ac84-ee226c128b1f>
2010-11-14 19:14:58 +0000

    moved iphone/ out of trunk and into its own branch

--- iphone/src/glv_buttons.cpp
+++
@@ -1,9 +0,0 @@
-/*	Graphics Library of Views (GLV) - GUI Building Toolkit
-	See COPYRIGHT file for authors and license information */
-
-#include "glv_buttons.h"
-
-namespace glv {
-
-
-}	//end namespace glv
