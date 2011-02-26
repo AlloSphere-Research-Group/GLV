e22e9c3d3f0f3add205dd960780b5fb8551ddbd1
l.putnam <l.putnam@db259aed-472b-0410-ac84-ee226c128b1f>
2011-02-26 22:46:42 +0000

    ColorSliders: fixed setters/getters

--- include/glv_color_controls.h
+++ include/glv_color_controls.h
@@ -91,19 +91,19 @@ public:
 		mComp2.anchor(0,1./num).stretch(1,1./num).set(0,1./num,0,0);
 		mComp3.anchor(0,2./num).stretch(1,1./num).set(0,2./num,0,0);
 		mCompA.anchor(0,3./num).stretch(1,1./num).set(0,3./num,0,0);
-		mComp1.value(1).style(&mSliderStyle).attach(updateComp, Update::Value, this);
-		mComp2.value(1).style(&mSliderStyle).attach(updateComp, Update::Value, this);
-		mComp3.value(1).style(&mSliderStyle).attach(updateComp, Update::Value, this);
-		mCompA.value(1).style(&mSliderStyle).attach(updateComp, Update::Value, this);
+		mComp1.setValue(1).style(&mSliderStyle).attach(updateComp, Update::Value, this);
+		mComp2.setValue(1).style(&mSliderStyle).attach(updateComp, Update::Value, this);
+		mComp3.setValue(1).style(&mSliderStyle).attach(updateComp, Update::Value, this);
+		mCompA.setValue(1).style(&mSliderStyle).attach(updateComp, Update::Value, this);
 		setColor();
 		(*this) << mComp1 << mComp2 << mComp3 << mCompA;
 		mCompA.property(Visible, mControlsAlpha);
 	}
 	
 	bool isHSV() const { return mIsHSV; }
-	const Color& value() const { return mColor; }
-	ColorSliders& value(const Color& v){ setColor(v); return *this; }
-	ColorSliders& value(const HSV& v){ setColor(v); return *this; }
+	const Color& getValue() const { return mColor; }
+	ColorSliders& setValue(const Color& v){ setColor(v); return *this; }
+	ColorSliders& setValue(const HSV& v){ setColor(v); return *this; }
 
 	virtual const char * className() const { return "ColorSliders"; }
 	
@@ -118,8 +118,8 @@ protected:
 		if(isHSV()){ HSV h=v; c1=h.h; c2=h.s; c3=h.v; }
 		else{ c1=v.r; c2=v.g; c3=v.b; }
 		// assuming value() does not send notification...
-		mComp1.value(c1); mComp2.value(c2); mComp3.value(c3);
-		mCompA.value(v.a);
+		mComp1.setValue(c1); mComp2.setValue(c2); mComp3.setValue(c3);
+		mCompA.setValue(v.a);
 		setColor();
 	}
 
@@ -128,16 +128,16 @@ protected:
 		if(isHSV()){ c1=v.h; c2=v.s; c3=v.v; }
 		else{ Color c=v; c1=c.r; c2=c.g; c3=c.b; }
 		// assuming value() does not send notification...
-		mComp1.value(c1); mComp2.value(c2); mComp3.value(c3);
+		mComp1.setValue(c1); mComp2.setValue(c2); mComp3.setValue(c3);
 		//mCompA.value(v.a);
 		setColor();
 	}
 
 	void setColor(){
-		float c1=mComp1.value(), c2=mComp2.value(), c3=mComp3.value();
+		float c1=mComp1.getValue(), c2=mComp2.getValue(), c3=mComp3.getValue();
 		if(isHSV()){	mColor = HSV(c1,c2,c3);}
 		else{			mColor.set(c1,c2,c3);}
-		if(mControlsAlpha) mColor.a = mCompA.value();
+		if(mControlsAlpha) mColor.a = mCompA.getValue();
 		mSliderStyle.color.fore = mColor;
 		//value().print();
 	}
