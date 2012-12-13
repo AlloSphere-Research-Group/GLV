039149242a04a8b7f1c7bf77c04addc5a17cc35f
LancePutnam <putnam.lance@gmail.com>
2012-12-13 17:39:31 +0100

    add text entry to NumberDialers

--- src/glv_textview.cpp
+++ src/glv_textview.cpp
@@ -132,276 +132,15 @@ void Label::rotateRect(){ t += w - h; transpose(); }
 
 
 
-#define CTOR_LIST mNI(0), mNF(0), mAcc(0), mShowSign(true), mOverwriteMode(true), mDimZero(false)
-#define CTOR_BODY\
-	font().letterSpacing(1./4);\
-	enable(DrawSelectionBox)
-
-NumberDialers::NumberDialers(int numInt, int numFrac, double max, double min, int nx, int ny)
-:	Widget(Rect(0,0, (12-2)*(numInt+numFrac+1), 12), 2, false,false,true), CTOR_LIST
-{
-	CTOR_BODY;
-	data().resize(Data::DOUBLE, nx, ny);
-	resize(numInt, numFrac);
-	dig(mNI);
-	fitExtent();
-	interval(max, min);
-} 
-
-NumberDialers::NumberDialers(const NumberDialers& v)
-:	Widget(v,2, false,false,true), CTOR_LIST
-{
-	CTOR_BODY;
-	data() = v.data();
-	data().clone();
-	dig(v.sizeInteger());
-	resize(v.sizeInteger(), v.sizeFraction());
-	interval(v.max(), v.min());
+bool TextView::filterNumeric(const std::string& text, int pos, int newChar){
+	return
+		((newChar >= '0') && (newChar <= '9'))
+		|| newChar=='-' || newChar=='+' || newChar=='e' || newChar=='.'
+	;
 }
-
-#undef CTOR_LIST
-#undef CTOR_BODY
-
-void NumberDialers::fitExtent(){
-	extent(
-		draw::pix(sizeX() * (paddingX()*2 + (numDigits() * font().advance('M'))) + 1),
-		draw::pix(sizeY() * (paddingY()*2 + font().cap()) + 1)
-	);
-//	print();
-}
-
-NumberDialers& NumberDialers::interval(double mx, double mn){
-	mInterval.endpoints(mn, mx);
-	double m = maxVal();	// do not allow numbers larger than can be displayed
-	if(min() < -m) mInterval.min(-m);
-	if(max() >  m) mInterval.max( m);
-	showSign(mn < 0);
-//	valSet(mVal);
-	for(int i=0; i<data().size(); ++i) setValue(getValue(i), i);
-	return *this;
-}
-
-NumberDialers& NumberDialers::resize(int numInt, int numFrac){
-	mNI = numInt; mNF = numFrac;
-//	mValMul = 1. / pow(10., mNF);
-//	setWidth();
-	fitExtent();
-	return *this;
-}
-
-NumberDialers& NumberDialers::showSign(bool v){
-	mShowSign=v;
-//	setWidth();
-	fitExtent();
-	return *this;
-}
-
-int NumberDialers::sizeFraction() const { return mNF; }
-int NumberDialers::sizeInteger() const { return mNI; }
-
-void NumberDialers::onDraw(GLV& g){ //printf("% g\n", value());
-	using namespace glv::draw;
-
-	fitExtent();
-
-	float dxCell= dx();
-	float dyCell= dy();
-	float dxDig = font().advance('M');
-
-//	View::enable(DrawSelectionBox);
-//	View::enable(DrawGrid);
-
-	// draw box at position (only if focused)
-	if(enabled(Focused)){
-	
-		float x = dxCell*selectedX() + paddingX()/1 - 1;
-		//float y = dyCell*selectedY() + paddingY()/2;
-		float y = dyCell*(selectedY()+0.5);
-		float ty= font().cap()/2. + 3;
-	
-//		color(colors().fore, colors().fore.a*0.4);
-		color(colors().selection);
-		//rectangle(bx + dig()*dxDig, by, bx + (dig()+1)*dxDig, by + dyCell-0.5f);
-		rectangle(x + dig()*dxDig, y-ty, x + (dig()+1)*dxDig, y+ty);
-	}
-
-	drawSelectionBox();
-	drawGrid(g.graphicsData());
-
-	lineWidth(1);
-
-	for(int i=0; i<sizeX(); ++i){
-		for(int j=0; j<sizeY(); ++j){
-			
-			float cx = dxCell*i;	// left edge of cell
-			float cy = dyCell*j;	// top edge of cell
-			
-			// draw number
-			int vali = valInt(i,j);
-			int absVal = vali < 0 ? -vali : vali;
-			int msd = mNF;	// position from right of most significant digit
-			
-			if(absVal > 0){
-				msd = (int)log10((double)absVal);
-				int p = numDigits() - (mShowSign ? 2:1);
-				msd = msd < mNF ? mNF : (msd > p ? p : msd);
-			}
-			
-			if(mNI == 0) msd-=1;
-
-			// Determine digit string
-			char str[32];
-			int ic = numDigits();
-			str[ic] = '\0';
-			for(int i=0; i<numDigits(); ++i) str[i]=' ';
-			
-			int power = 1;
-			bool drawChar = false; // don't draw until non-zero or past decimal point
-
-			if(mShowSign && vali < 0) str[0] = '-';
-			
-			for(int i=0; i<=msd; ++i){
-				char c = '0' + (absVal % (power*10))/power;
-				power *= 10;
-				if(c!='0' || i>=mNF) drawChar = true;
-				--ic;
-				if(drawChar) str[ic] = c;
-			}
-
-			// Draw the digit string
-			float tx = int(cx + paddingX());
-			float ty = int(cy + paddingY());
-
-			if(vali || !dimZero()){
-				color(colors().text);
-			} else {
-				color(colors().text.mix(colors().back, 0.8));
-			}
-		//	printf("%s\n", str);
-//			font().render(g.graphicsData(), str, pixc(tx), pixc(ty));
-//			if(mNF>0) font().render(g.graphicsData(), ".", pixc(dxDig*(mNI+numSignDigits()-0.5f) + tx), pixc(ty));
-			font().render(g.graphicsData(), str, tx, ty);
-			if(mNF>0) font().render(g.graphicsData(), ".", dxDig*(mNI+numSignDigits()-0.5f) + tx, ty);
-
-		}
-	}
-}
-
-bool NumberDialers::onEvent(Event::t e, GLV& g){
-
-	if(!Widget::onEvent(e,g)) return false;
-
-	const Keyboard& k = g.keyboard();
-	const Mouse& m    = g.mouse();
-
-	switch(e){
-	case Event::MouseDown:{
-		mAcc = 0;
-		int oldDig = dig();
-
-		selectFromMousePos(g);
-		float dxDig = font().advance('M');
-		int d = (m.xRel() - (dx() * selectedX() + paddingX())) / dxDig;
-		
-//		printf("%2d (%2d, %2d)\n", d, selectedX(), selectedY());
-		dig(d);
-		if(dig() == 0 && oldDig == 0 && mShowSign) flipSign();
-		return false;
-	}
-	
-	case Event::MouseDrag:
-		if(onNumber()){
-			mAcc += 0.25f * fabs(m.dy());
-			if(mAcc > 1){
-				int mul = (int)mAcc;
-				valAdd((m.dy() > 0.f ? -mag() : mag())*mul);
-				mAcc -= mul;
-			}
-		}
-		return false;
-	
-	case Event::KeyDown:
-		if(k.ctrl() || k.alt() || k.meta()){}
-		else if(k.isNumber() && onNumber()){
-			int vali = valInt(selected());
-			int v = vali < 0 ? -vali : vali;
-			int p = pow(10., numDigits()-1-dig());
-			v += (k.keyAsNumber() - ((v / p) % 10)) * p;
-			v = vali < 0 ? -v : v;
-			setValue(v * pow(10., -mNF));
-			if(!mOverwriteMode) dig(dig()+1);
-			return false;
-		}
-		else{
-			switch(k.key()){
-			case 'a': onNumber() ? valAdd( mag()) : flipSign(); return false;
-			case 'z': onNumber() ? valAdd(-mag()) : flipSign(); return false;
-			case '-': flipSign(); return false;
-			case 'c': setValue(0); return false;
-			case '.': dig(numDigits()-mNF); return false; // go to first fraction digit (if any)
-			case Key::Left:
-			case 's': dig(dig()-1); return false;
-			case Key::Right:
-			case 'd': dig(dig()+1); return false;
-			case 'o': mOverwriteMode^=true; return false;
-			}
-		}
-		break;
-		
-	default:;
-	}
-
-	return true;
-}
-
-// Old from NumberDialers:
-//	int mVal;				// current fixed-point value
-//	double mValMul;
-
-//	void setWidth(){ w = (h-2)*size(); }
-	
-//	int convert(double v) const { return (v/mValMul) + (v>0. ? 0.5:-0.5); }
-//	int mag() const { return pow(10., numDigits()-1-dig()); }
-
-//	void valAdd(int v){	valSet(v + mVal); }
-//	int valInt(int ix, int iy=0){ return convert(data().at<double>(ix,iy)); }
-//	void flipSign(){
-//		if((mVal>0 && -mVal>=convert(mMin)) || (mVal<0 && -mVal<=convert(mMax)))
-//			valSet(-mVal);
-//	}
-
-//	void valSet(int v){	// converts fixed point to floating point value
-////		mVal = glv::clip(v, convert(mMax), convert(mMin));
-////		double val = mVal * mValMul;
-////		Widget::setValue(val);
-//
-//		double val = v * mValMul;
-//		setValue(val);
-//	}
-
-//	NumberDialers& setValue(double v){ valSet(convert(v)); return *this; }
-
-//	virtual bool onAssignData(Data& d, int ind1, int ind2){
-//	
-////		double v = d.at<double>(ind1, ind2);		
-////		int vi = convert(v);
-//////		mVal = glv::clip(v, convert(mMax), convert(mMin));
-//////		double val = mVal * mValMul;		
-////		mVal = vi;
-//	
-//		if(Widget::onAssignData(d, ind1, ind2)){
-//			double v = d.at<double>(ind1, ind2);
-//			mVal = convert(v); //printf("%d\n", mVal);
-//			return true;
-//		}
-//		return false;
-//	}
-
-
-
 
 TextView::TextView(const Rect& r, float textSize)
-:	Widget(r, 4), mSpacing(1), mSel(0), mBlink(0)
+:	Widget(r, 4), mSpacing(1), mSel(0), mBlink(0), mFilter(NULL)
 {
 	data().resize(Data::STRING);
 	cursorPos(0);
@@ -489,11 +228,32 @@ bool TextView::onEvent(Event::t e, GLV& g){
 				}
 			}
 			else if(k.alt() || k.meta()){} // bubble if control key down
+
 			else if(k.isPrint()){
-				deleteSelected();
-				mText.insert(mPos, 1, k.key()); setValue(mText);
-				cursorPos(mPos+1);
-				return false;
+
+				// preserve current text if something is selected
+				std::string oldText;
+				int oldSel = mSel;
+				int oldPos = mPos;
+				if(mSel){
+					oldText = mText;
+					deleteSelected();
+				}
+
+				// No filter assigned or filter returns true
+				if(!mFilter || mFilter(getValue(), mPos, key)){
+					mText.insert(mPos, 1, k.key());
+					setValue(mText);
+					cursorPos(mPos+1);
+					return false;
+				}
+
+				// restore old text if the new character is invalid
+				if(!oldText.empty()){
+					mText = oldText;
+					mPos = oldPos;
+					mSel = oldSel;
+				}
 			}
 			else{
 				switch(key){
@@ -1065,6 +825,300 @@ bool SearchBox::ItemList::onEvent(Event::t e, GLV& g){
 	}
 	return ListView::onEvent(e,g);
 }
+
+
+
+TextView NumberDialers::mTextEntry;
+
+#define CTOR_LIST\
+	mNI(0), mNF(0), mAcc(0),\
+	mShowSign(true), mOverwriteMode(true), mDimZero(false), mTextEntryMode(false)
+#define CTOR_BODY\
+	font().letterSpacing(1./4);\
+	enable(DrawSelectionBox);\
+	mTextEntry.filter(TextView::filterNumeric).paddingY(2)
+
+NumberDialers::NumberDialers(int numInt, int numFrac, double max, double min, int nx, int ny)
+:	Widget(Rect(0,0, (12-2)*(numInt+numFrac+1), 12), 2, false,false,true), CTOR_LIST
+{
+	CTOR_BODY;
+	data().resize(Data::DOUBLE, nx, ny);
+	resize(numInt, numFrac);
+	dig(mNI);
+	fitExtent();
+	interval(max, min);
+} 
+
+NumberDialers::NumberDialers(const NumberDialers& v)
+:	Widget(v,2, false,false,true), CTOR_LIST
+{
+	CTOR_BODY;
+	data() = v.data();
+	data().clone();
+	dig(v.sizeInteger());
+	resize(v.sizeInteger(), v.sizeFraction());
+	interval(v.max(), v.min());
+}
+
+#undef CTOR_LIST
+#undef CTOR_BODY
+
+void NumberDialers::fitExtent(){
+	extent(
+		draw::pix(sizeX() * (paddingX()*2 + (numDigits() * font().advance('M'))) + 1),
+		draw::pix(sizeY() * (paddingY()*2 + font().cap()) + 1)
+	);
+//	print();
+}
+
+NumberDialers& NumberDialers::interval(double mx, double mn){
+	mInterval.endpoints(mn, mx);
+	double m = maxVal();	// do not allow numbers larger than can be displayed
+	if(min() < -m) mInterval.min(-m);
+	if(max() >  m) mInterval.max( m);
+	showSign(mn < 0);
+//	valSet(mVal);
+	for(int i=0; i<data().size(); ++i) setValue(getValue(i), i);
+	return *this;
+}
+
+NumberDialers& NumberDialers::resize(int numInt, int numFrac){
+	mNI = numInt; mNF = numFrac;
+//	mValMul = 1. / pow(10., mNF);
+//	setWidth();
+	fitExtent();
+	return *this;
+}
+
+NumberDialers& NumberDialers::showSign(bool v){
+	mShowSign=v;
+//	setWidth();
+	fitExtent();
+	return *this;
+}
+
+int NumberDialers::sizeFraction() const { return mNF; }
+int NumberDialers::sizeInteger() const { return mNI; }
+
+void NumberDialers::onDraw(GLV& g){ //printf("% g\n", value());
+	using namespace glv::draw;
+
+	fitExtent();
+
+	float dxCell= dx();
+	float dyCell= dy();
+	float dxDig = font().advance('M');
+
+//	View::enable(DrawSelectionBox);
+//	View::enable(DrawGrid);
+
+	// draw box at position (only if focused)
+	if(enabled(Focused)){
+
+		float x = dxCell*selectedX() + paddingX()/1 - 1;
+		//float y = dyCell*selectedY() + paddingY()/2;
+		float y = dyCell*(selectedY()+0.5);
+		float ty= font().cap()/2. + 3;
+
+//		color(colors().fore, colors().fore.a*0.4);
+		color(colors().selection);
+		//rectangle(bx + dig()*dxDig, by, bx + (dig()+1)*dxDig, by + dyCell-0.5f);
+		rectangle(x + dig()*dxDig, y-ty, x + (dig()+1)*dxDig, y+ty);
+	}
+
+	drawSelectionBox();
+	drawGrid(g.graphicsData());
+
+	lineWidth(1);
+
+	if(mTextEntryMode){
+		mTextEntry.extent(dxCell, dyCell);
+		mTextEntry.pos(dxCell*selectedX(), dyCell*selectedY());
+	}
+
+	for(int i=0; i<sizeX(); ++i){
+		for(int j=0; j<sizeY(); ++j){
+
+			float cx = dxCell*i;	// left edge of cell
+			float cy = dyCell*j;	// top edge of cell
+
+			// draw number
+			int vali = valInt(i,j);
+			int absVal = vali < 0 ? -vali : vali;
+			int msd = mNF;	// position from right of most significant digit
+
+			if(absVal > 0){
+				msd = (int)log10((double)absVal);
+				int p = numDigits() - (mShowSign ? 2:1);
+				msd = msd < mNF ? mNF : (msd > p ? p : msd);
+			}
+
+			if(mNI == 0) msd-=1;
+
+			// Determine digit string
+			char str[32];
+			int ic = numDigits();
+			str[ic] = '\0';
+			for(int i=0; i<numDigits(); ++i) str[i]=' ';
+
+			int power = 1;
+			bool drawChar = false; // don't draw until non-zero or past decimal point
+
+			if(mShowSign && vali < 0) str[0] = '-';
+
+			for(int i=0; i<=msd; ++i){
+				char c = '0' + (absVal % (power*10))/power;
+				power *= 10;
+				if(c!='0' || i>=mNF) drawChar = true;
+				--ic;
+				if(drawChar) str[ic] = c;
+			}
+
+			// Draw the digit string
+			float tx = int(cx + paddingX());
+			float ty = int(cy + paddingY());
+
+			if(vali || !dimZero()){
+				color(colors().text);
+			} else {
+				color(colors().text.mix(colors().back, 0.8));
+			}
+		//	printf("%s\n", str);
+//			font().render(g.graphicsData(), str, pixc(tx), pixc(ty));
+//			if(mNF>0) font().render(g.graphicsData(), ".", pixc(dxDig*(mNI+numSignDigits()-0.5f) + tx), pixc(ty));
+			font().render(g.graphicsData(), str, tx, ty);
+			if(mNF>0) font().render(g.graphicsData(), ".", dxDig*(mNI+numSignDigits()-0.5f) + tx, ty);
+		}
+	}
+}
+
+bool NumberDialers::onEvent(Event::t e, GLV& g){
+
+	if(!Widget::onEvent(e,g)) return false;
+
+	const Keyboard& k = g.keyboard();
+	const Mouse& m    = g.mouse();
+
+	switch(e){
+	case Event::MouseDown:{
+		mAcc = 0;
+		int oldDig = dig();
+
+		selectFromMousePos(g);
+		float dxDig = font().advance('M');
+		int d = (m.xRel() - (dx() * selectedX() + paddingX())) / dxDig;
+		
+//		printf("%2d (%2d, %2d)\n", d, selectedX(), selectedY());
+		dig(d);
+		if(dig() == 0 && oldDig == 0 && mShowSign) flipSign();
+		return false;
+	}
+	
+	case Event::MouseDrag:
+		if(onNumber()){
+			mAcc += 0.25f * fabs(m.dy());
+			if(mAcc > 1){
+				int mul = (int)mAcc;
+				valAdd((m.dy() > 0.f ? -mag() : mag())*mul);
+				mAcc -= mul;
+			}
+		}
+		return false;
+	
+	case Event::KeyDown:
+		if(k.ctrl() || k.alt() || k.meta()){}
+		else if(k.isNumber() && onNumber()){
+			int vali = valInt(selected());
+			int v = vali < 0 ? -vali : vali;
+			int p = pow(10., numDigits()-1-dig());
+			v += (k.keyAsNumber() - ((v / p) % 10)) * p;
+			v = vali < 0 ? -v : v;
+			setValue(v * pow(10., -mNF));
+			if(!mOverwriteMode) dig(dig()+1);
+			return false;
+		}
+		else{
+			switch(k.key()){
+			case 'a': onNumber() ? valAdd( mag()) : flipSign(); return false;
+			case 'z': onNumber() ? valAdd(-mag()) : flipSign(); return false;
+			case '-': flipSign(); return false;
+			case 'c': setValue(0); return false;
+			case '=':
+			//case Key::Return:
+				if(!mTextEntryMode){ // bring up the text entry box
+					mTextEntryMode=true;
+					(*this) << mTextEntry;
+					mTextEntry.setValue(toString(getValue()));
+					mTextEntry.selectAll();
+					g.setFocus(&mTextEntry);
+				}
+				else{				// set value from text entry box, then remove it
+					mTextEntryMode=false;
+					g.setFocus(this);
+					mTextEntry.remove();
+					if(!mTextEntry.getValue().empty()){				
+						setValue(mTextEntry.data().at<double>(0));
+					}
+				}
+				return false;
+			case '.': dig(numDigits()-mNF); return false; // go to first fraction digit (if any)
+			case Key::Left:
+			case 's': dig(dig()-1); return false;
+			case Key::Right:
+			case 'd': dig(dig()+1); return false;
+			case 'o': mOverwriteMode^=true; return false;
+			}
+		}
+		break;
+
+	default:;
+	}
+
+	return true;
+}
+
+// Old from NumberDialers:
+//	int mVal;				// current fixed-point value
+//	double mValMul;
+
+//	void setWidth(){ w = (h-2)*size(); }
+	
+//	int convert(double v) const { return (v/mValMul) + (v>0. ? 0.5:-0.5); }
+//	int mag() const { return pow(10., numDigits()-1-dig()); }
+
+//	void valAdd(int v){	valSet(v + mVal); }
+//	int valInt(int ix, int iy=0){ return convert(data().at<double>(ix,iy)); }
+//	void flipSign(){
+//		if((mVal>0 && -mVal>=convert(mMin)) || (mVal<0 && -mVal<=convert(mMax)))
+//			valSet(-mVal);
+//	}
+
+//	void valSet(int v){	// converts fixed point to floating point value
+////		mVal = glv::clip(v, convert(mMax), convert(mMin));
+////		double val = mVal * mValMul;
+////		Widget::setValue(val);
+//
+//		double val = v * mValMul;
+//		setValue(val);
+//	}
+
+//	NumberDialers& setValue(double v){ valSet(convert(v)); return *this; }
+
+//	virtual bool onAssignData(Data& d, int ind1, int ind2){
+//	
+////		double v = d.at<double>(ind1, ind2);		
+////		int vi = convert(v);
+//////		mVal = glv::clip(v, convert(mMax), convert(mMin));
+//////		double val = mVal * mValMul;		
+////		mVal = vi;
+//	
+//		if(Widget::onAssignData(d, ind1, ind2)){
+//			double v = d.at<double>(ind1, ind2);
+//			mVal = convert(v); //printf("%d\n", mVal);
+//			return true;
+//		}
+//		return false;
+//	}
 
 
 
